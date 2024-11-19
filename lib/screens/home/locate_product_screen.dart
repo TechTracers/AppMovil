14bd1bda66092b1cd02ceb4fd6b10c1f7ea765e2
Davila Yoimer <89022117+Yoimer-Davila@users.noreply.github.com>
2024-11-19 17:15:04 -0500

    fixed markers

--- lib/screens/home/locate_product_screen.dart
+++ lib/screens/home/locate_product_screen.dart
@@ -20,26 +20,58 @@ class _LocateProductScreenState extends State<LocateProductScreen> {
   final FirebaseService firebaseService =
       FirebaseService(FirebaseDatabase.instance.ref());
   Location? productLocation;
-  bool isLoading = true;
+  Position? position;
+
+  final MapController _mapController = MapController();
+  double _currentZoom = 15.0;
+  bool _isPositionLoaded = false;
+  bool _isProductLoaded = false;
+
+  bool get isLoading => _isPositionLoaded == false || _isProductLoaded == false;
 
   @override
   void initState() {
     super.initState();
     listenToLocations();
+    getCurrentPosition();
+  }
+
+  void getCurrentPosition() async {
+    final position = await determineCurrentPosition();
+    setState(() {
+      this.position = position;
+      _isPositionLoaded = true;
+    });
   }
 
   void listenToLocations() {
     firebaseService.getLocations(widget.iotUID).listen((locations) {
       setState(() {
         productLocation = locations;
-        isLoading = false; // Detener el indicador de carga
+        _isProductLoaded = true;
       });
     }, onError: (error) {
-      setState(() {
-        isLoading = false; // Detener el indicador de carga en caso de error
-      });
+      setState(() {});
       print("Error al escuchar datos de Firebase: $error");
     });
+  }
+
+  _changeZoom(int value) {
+    final zoom = (_currentZoom + value).clamp(1.0, 21.0);
+    if (zoom == _currentZoom) return;
+    _currentZoom = zoom;
+    _mapController.move(_mapController.camera.center, _currentZoom);
+  }
+
+  void _centerMap() {
+    if (productLocation != null && position != null) {
+      final bounds = LatLngBounds.fromPoints([
+        LatLng(productLocation!.latitude, productLocation!.longitude),
+        LatLng(position!.latitude, position!.longitude),
+      ]);
+      _mapController.fitCamera(CameraFit.bounds(
+          bounds: bounds, maxZoom: 21, padding: const EdgeInsets.all(20)));
+    }
   }
 
   @override
@@ -50,72 +82,85 @@ class _LocateProductScreenState extends State<LocateProductScreen> {
       ),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
-          : productLocation == null
-              ? const Center(
-                  child: Text(
-                    'No se encontraron ubicaciones válidas.',
-                    style: TextStyle(fontSize: 16),
+          : Stack(
+              children: [
+                FlutterMap(
+                  mapController: _mapController,
+                  options: MapOptions(
+                    initialCenter: LatLng(
+                      productLocation!.latitude,
+                      productLocation!.longitude,
+                    ),
+                    onPositionChanged: (position, gesture) {
+                      _currentZoom = position.zoom;
+                    },
+                    initialZoom: _currentZoom,
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate:
+                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: const ['a', 'b', 'c'],
+                    ),
+                    MarkerLayer(
+                      markers: [
+                        Marker(
+                          point: LatLng(productLocation!.latitude,
+                              productLocation!.longitude),
+                          width: 80,
+                          height: 80,
+                          child: const Icon(
+                            Icons.location_pin,
+                            color: Colors.red,
+                            size: 40.0,
+                          ),
+                        ),
+                        Marker(
+                          point:
+                              LatLng(position!.latitude, position!.longitude),
+                          width: 80,
+                          height: 80,
+                          child: const Icon(
+                            Icons.location_pin,
+                            color: Colors.green,
+                            size: 40.0,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Column(
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () => _changeZoom(1),
+                        child: const Icon(Icons.zoom_in, color: Colors.black),
+                      ),
+                      const SizedBox(height: 5),
+                      FloatingActionButton(
+                        onPressed: () => _changeZoom(-1),
+                        child: const Icon(
+                          Icons.zoom_out,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      FloatingActionButton(
+                        onPressed: _centerMap,
+                        backgroundColor: Colors.white,
+                        child: const Icon(Icons.center_focus_strong,
+                            color: Colors.black),
+                      )
+                    ],
                   ),
                 )
-              : FutureBuilder<Position>(
-                  future: determineCurrentPosition(),
-                  builder:
-                      (BuildContext context, AsyncSnapshot<Position> snapshot) {
-                    if (snapshot.hasError || snapshot.connectionState != ConnectionState.done || snapshot.data == null) {
-                      return const Center(
-                        child: CircularProgressIndicator(
-                        ),
-                      );
-                    }
-                    if(snapshot.connectionState == ConnectionState.done && snapshot.data == null) {
-                      // return algo para indicar que paso algo
-                    }
-
-                    final position = snapshot.data!;
-                    return FlutterMap(
-                      options: MapOptions(
-                        initialCenter: LatLng(
-                          productLocation!.latitude,
-                          productLocation!.longitude,
-                        ),
-                        initialZoom: 15.0,
-                      ),
-                      children: [
-                        TileLayer(
-                          urlTemplate:
-                              "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                          subdomains: ['a', 'b', 'c'],
-                        ),
-                        MarkerLayer(
-                          markers: [
-                            Marker(
-                              point: LatLng(productLocation!.latitude,
-                                  productLocation!.longitude),
-                              width: 80,
-                              height: 80,
-                              child: const Icon(
-                                Icons.location_pin,
-                                color: Colors.red,
-                                size: 40.0,
-                              ),
-                            ),
-                            Marker(
-                              point:
-                                  LatLng(position.latitude, position.longitude),
-                              width: 80,
-                              height: 80,
-                              child: const Icon(
-                                Icons.location_pin,
-                                color: Colors.green,
-                                size: 40.0,
-                              ),
-                            )
-                          ],
-                        ),
-                      ],
-                    );
-                  },
-                ),
+              ],
+            ),
     );
   }
 }
